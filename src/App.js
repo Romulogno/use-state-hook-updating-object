1ef07f8861ba3768cc52b24c11dd2096707249a0
Romulogno <70775002+Romulogno@users.noreply.github.com>
2023-03-14 14:03:13 +0000

    use state with objects exercice 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  //State with a object with two itens inside
+    const [trabalho, setTrabalho] = useState(
+      {
+        position:"DevOps",
+        empresa:"Accenture"
+       }
+     );
+    const [newPosition, setNewPosition] = useState({});
+    const [newEmpresa, setNewEmpresa] = useState({}) 
+
+      //function that make a copy of the state and update the item necessary
+       function carrerChanging(){
+        const copiaTrabalho = {...trabalho};
+        copiaTrabalho.position = newPosition;
+        setTrabalho(copiaTrabalho);
+        
+      //Versao seu o Onchange input
+       /* copiaTrabalho.empresa = "Frontiers"
+        copiaTrabalho.position = "FrontEnd Developer";
+        setTrabalho(copiaTrabalho)
+        */
+       }
+
+       function empresaChanging(){
+        const copiaTrabalho = {...trabalho};
+        copiaTrabalho.empresa = newEmpresa;
+        setTrabalho(copiaTrabalho)
+        console.log(copiaTrabalho)
+       }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Empresa: {trabalho.empresa}</h1>
+      <h1> Position: {trabalho.position}</h1>
+      <input type="text"
+       onChange={(event) => {setNewPosition(event.target.value)}}
+      />
+      <button 
+      onClick={carrerChanging}
+      >
+     Atualiza trabalho
+      </button>
+      <br/>
+      <input type="text"
+       onChange={(event) => {setNewEmpresa(event.target.value)}}
+      />
+      <button onClick={empresaChanging}>Atualizar Empresa</button>
     </div>
   );
 }
